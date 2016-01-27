6a8234ea1268b2e65efe306f54b61e7ad7d06077
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-01-27 22:34:46 +0800

    再修改一下登录功能

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use Auth;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Auth\Registrar;
 
 class LoginController extends Controller {
 
@@ -18,7 +16,7 @@ class LoginController extends Controller {
 
 
 
-        if ($this->auth->attempt(['email' => $request['email'], 'password' => $request['password'],'state' => 1],$request->has('remember')))
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password'],'state' => 1],$request->has('remember')))
         {
             return redirect()->intended('home');
         }
@@ -26,17 +24,9 @@ class LoginController extends Controller {
 
     public function getLogout()
 	{
-		$this->auth->logout();
+		Auth::logout();
 
 		return redirect('/');
 	}
 
-
-	public function __construct(Guard $auth, Registrar $registrar)
-	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
-
-		//$this->middleware('guest', ['except' => 'getLogout']);
-	}
 }

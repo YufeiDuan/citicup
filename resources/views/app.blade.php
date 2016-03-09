2db60884d15229fe0a07d7934f88fd928afc8147
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-03-09 22:02:15 +0800

    修改：字体，fonts.useso.com 360网站卫士代理访问

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <htm>
 <head>
-	@yield('head')
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="renderer" content="webkit">
+	@yield('head')
 	<title>2016花旗杯</title>
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/style.css') }}" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('/css/jquery-ui.min.css') }}" rel="stylesheet">
 	<!-- Fonts -->
+	<link href='//fonts.useso.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
--- resources/views/reg.blade.php
+++ resources/views/reg.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <htm>
 	<head>
-
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="renderer" content="webkit">
 		<title>2016花旗杯</title>
 		
-		<!--<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>-->
 		<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 		<link href="{{ asset('/css/jquery-ui.min.css') }}" rel="stylesheet">
 		<script src="/js/jquery.min.js"></script>
 		<script src="/js/bootstrap.min.js"></script>
 		<script src="/js/jquery-ui.min.js"></script>
+		<!-- Fonts -->
+		<link href='//fonts.useso.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
 
 		@yield('head')
 

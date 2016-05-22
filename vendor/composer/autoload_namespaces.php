8a79fabf4c0f5653bfb7a64a9ab0ea8812ae0c48
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-05-22 23:10:31 +0800

    add: zipper

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -29,4 +29,5 @@ return array(
     'Doctrine\\Common\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib'),
     'Diff' => array($vendorDir . '/phpspec/php-diff/lib'),
     'Cron' => array($vendorDir . '/mtdowling/cron-expression/src'),
+    'Chumper\\Zipper' => array($vendorDir . '/chumper/zipper/src'),
 );

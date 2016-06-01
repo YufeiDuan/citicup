aaecb10ec095ceec7591c2809d1ddc68140b1702
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-06-01 13:17:20 +0800

    fix: ziparchive chinese filename

--- app/Hzip.php
+++ app/Hzip.php
@@ -10,31 +10,34 @@ class HZip
     while (false !== $f = readdir($handle)) { 
       if ($f != '.' && $f != '..') { 
         $filePath = "$folder/$f"; 
+
         // Remove prefix from file path before add to zip. 
         $localPath = substr($filePath, $exclusiveLength); 
-        if (is_file($filePath)) { 
-          $zipFile->addFile($filePath, $localPath); 
-        } elseif (is_dir($filePath)) { 
-          $zipFile->addEmptyDir($localPath); 
-          self::folderToZip($filePath, $zipFile, $exclusiveLength); 
+        if (is_file($filePath)) {
+          //$codepage = 'Windows-1252' . trim(strstr(setlocale(LC_CTYPE, 0), '.'), '.');
+          $localPath = iconv('utf-8', 'GB2312//IGNORE', $localPath);
+          $zipFile->addFile($filePath, $localPath);
+        } elseif (is_dir($filePath)) {
+          $zipFile->addEmptyDir($localPath);
+          self::folderToZip($filePath, $zipFile, $exclusiveLength);
         }
       }
-    } 
-    closedir($handle); 
+    }
+    closedir($handle);
   } 
   
-  public static function zipDir($sourcePath, $outZipPath) 
-  { 
-    $pathInfo = pathInfo($sourcePath); 
-    $parentPath = $pathInfo['dirname']; 
-    $dirName = $pathInfo['basename']; 
+  public static function zipDir($sourcePath, $outZipPath)
+  {
+    $pathInfo = pathInfo($sourcePath);
+    $parentPath = $pathInfo['dirname'];
+    $dirName = $pathInfo['basename'];
     //$dirName = iconv('UTF-8','UTF-8',$dirName);
     $sourcePath=$parentPath.'/'.$dirName;
-    $z = new ZipArchive(); 
+    $z = new ZipArchive();
     $z->open($outZipPath, ZIPARCHIVE::CREATE);
     $z->addEmptyDir($dirName);
     self::folderToZip($sourcePath, $z, strlen("$parentPath/")); 
-    $z->close(); 
-  } 
-} 
+    $z->close();
+  }
+}
 ?>

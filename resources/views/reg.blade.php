eed0bdd6af17348690efa2e5de4bd81093625e48
Duan Yu Fei <duanyufeichn@foxmail.com>
2016-06-02 19:58:14 +0800

    fix: page title for register

--- resources/views/reg.blade.php
+++ resources/views/reg.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="renderer" content="webkit">
-	<title>2016 CitiCup</title>
+	<title>2016 Citi Financial Innovation Application Competition</title>
 	
 	<link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">

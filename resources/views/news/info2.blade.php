899df1c5475bc0979e5ba4558517968eb3b06067
DUAN Yufei <duanyufeichn@gmail.com>
2016-10-17 20:11:24 +0800

    发布新闻前20

---
+++ resources/views/news/info2.blade.php
@@ -0,0 +1,140 @@
+@extends('news.info')
+@section('title')
+本届“花旗杯”金融创新应用大赛预赛前20强团队现已产生
+@endsection
+
+@section('source')
+来源：花旗杯组委会&nbsp;&nbsp;&nbsp;&nbsp;日期：2016-10-17
+@endsection
+
+@section('content')
+	<p>经过大赛组委会评委的认真评审、打分，组委会工作人员的核对、统计，本届“花旗杯”金融创新应用大赛预赛前20强团队现已产生。</p>
+	<table class="table table-bordered" align="center" id="schedule">
+    <tr>
+						<th>排名<br>Rank</th>
+						<th>团队名称<br>Team</th>
+						<th>作品名称<br>Project</th>
+            <th>学校<br>University</th>
+		</tr>
+    <tr>
+            <td>1</td>
+            <td>Forbes</td>
+            <td>易融——中小企业资产支持证券融资交易一体化平台</td>
+            <td>东北财经大学</td>
+    </tr>
+    <tr>
+            <td>2</td>
+            <td>Antares</td>
+            <td>期货保险组合管理系统</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>3</td>
+            <td>future fofer</td>
+            <td>fof量化投资交易管理系统</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>4</td>
+            <td>Miracle旗迹</td>
+            <td>“Q-Executor”A股订单智能优化执行系统</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>5</td>
+            <td>随物应机</td>
+            <td>智随保—基于地理位置的智能保险服务</td>
+            <td>西安交通大学</td>
+    </tr>
+    <tr>
+            <td>6</td>
+            <td>go-getter</td>
+            <td>易车宝——基于区块链存储技术的二手车交易及贷款平台</td>
+            <td>四川大学</td>
+    </tr>
+    <tr>
+            <td>7</td>
+            <td>VR the future</td>
+            <td>估估网——虚拟商品价值评估平台</td>
+            <td>武汉大学</td>
+    </tr>
+    <tr>
+            <td>8</td>
+            <td>Finance Panda</td>
+            <td>基于比特币的跨国实时汇款平台</td>
+            <td>西南财经大学</td>
+    </tr>
+    <tr>
+            <td>9</td>
+            <td>Soft-business</td>
+            <td>基于人工智能的LOF基金套利</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>10</td>
+            <td>Pro-MAX</td>
+            <td>汇盈-企业收付汇风险控制平台</td>
+            <td>东北财经大学</td>
+    </tr>
+    <tr>
+            <td>11</td>
+            <td>WHUBang</td>
+            <td>Stockbots移动端程序化股票交易平台</td>
+            <td>武汉大学</td>
+    </tr>
+    <tr>
+            <td>12</td>
+            <td>Invest Go</td>
+            <td>金诚智能投顾平台（简称：金诚智投）</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>13</td>
+            <td>LordAlpha</td>
+            <td>外汇辅助决策系统</td>
+            <td>武汉大学/湖北美术学院/江西财经大学</td>
+    </tr>
+    <tr>
+            <td>14</td>
+            <td>Alpha Seeker</td>
+            <td>Share Futures 股指预测软件</td>
+            <td>武汉大学</td>
+    </tr>
+    <tr>
+            <td>15</td>
+            <td>Broker</td>
+            <td>翼贷通——线上银行贷款申请平台</td>
+            <td>大连理工大学</td>
+    </tr>
+    <tr>
+            <td>16</td>
+            <td>Compass</td>
+            <td>多商品组合的期货跨期套利交易系统</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>17</td>
+            <td>Game of Finance</td>
+            <td>Quant+——量化投资社区平台</td>
+            <td>南京大学</td>
+    </tr>
+    <tr>
+            <td>18</td>
+            <td>The Best Team</td>
+            <td>“亲和一键”---针对老年人群体的银行服务移动应用</td>
+            <td>西安交通大学</td>
+    </tr>
+    <tr>
+            <td>19</td>
+            <td>Crazyfish</td>
+            <td>基于风险偏好与风险承受能力考量的个性化理财产品推荐平台</td>
+            <td>中山大学</td>
+    </tr>
+    <tr>
+            <td>20</td>
+            <td>31339</td>
+            <td>声声赚</td>
+            <td>上海财经大学</td>
+    </tr>
+  </table>
+@endsection
